bba28d5d8b681393854123d1f23d58ad5fdc5e1b
WINDOWS-M2UFRC1\eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-02-19 01:27:06 +0200

    First Video added! Hooooray!

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from  './components/search_bar';
 import VideoList from  './components/video_list';
+import VideoDetail from  './components/video_detail';
 
 const API_KEY = 'AIzaSyCs-xGMalkiyWH86FwhaLzfaeQWBwZxrrA';
 
@@ -10,23 +11,20 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = { videos: []};
-
     //  init the plugin
-    YTSearch({key: API_KEY, term: 'yoga'}, (videos) => {
+    YTSearch({key: API_KEY, term: 'trend'}, (videos) => {
       this.setState({videos});
       // -- > this.setState({videos: videos});
     })
-
   }
   render(){
     return (
         <div>
             <SearchBar />
+            <VideoDetail video={this.state.videos[0]}/>
             <VideoList videos={this.state.videos}/>
         </div>
     )
   }
-
 };
-
 ReactDOM.render(<App />, document.querySelector('.container'));
